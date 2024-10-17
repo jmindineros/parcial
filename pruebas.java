a0c626b51c3305e003db7535c24e9c8bab0cecd4
jmindineros <jmindineros@estudiante.uniajc.edu.co>
2024-10-17 18:56:23 +0000

    calcular_media_pareja_conerrores

--- pruebas.java
+++ pruebas.java
@@ -5,25 +5,28 @@ public class pruebas {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = 0;
-        int num;
-        int max;
-        int min;
-        System.out.println("escriba el numero");
-        num = sc.nextInt();
-        min = num;
-        max = num;
+        Double m;
+        Double max;
+        Double n1;
+        Double n2;
 
-        for (int i = 1; i < 100;i++) {
-            System.out.println("escriba el num");
-            num=sc.nextInt();
-            if (num > max) {
-                max = num;
-            } else if (num < min) {
-                min = num;
-            }
+        System.out.println("escriba el primer numero");
+        n1 = sc.nextDouble();
+        System.out.println("escriba el segundo numero");
+        n2 = sc.nextDouble();
+        max = (n1 + n2)/2;
+
+        while (n1 != 999 || n2 != 999)
+
+        System.out.println("escriba el primer numero");
+        n1 = sc.nextDouble();
+        System.out.println("escriba el segundo numero");
+        n2 = sc.nextDouble();
+        m = (n1 + n2)/2;
+        if (m>max){
+            max = m;
         }
+        System.out.println("la media maxima" + max);
 
-        System.out.println("maximo " + max + " minimo " + min);
     }
 }
