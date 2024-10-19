de04700a963b897c2c0ab510b76b7ab00c5dbff2
jmindineros <jmindineros@estudiante.uniajc.edu.co>
2024-10-19 21:14:58 +0000

    ejer 5.19

--- pruebas.java
+++ pruebas.java
@@ -5,20 +5,24 @@ public class pruebas {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int sw;
-        Double n;
-
-        sw = 0;
-        while (sw == 0) {
-            System.out.println("ingrese n");
-            n = sc.nextDouble();
-            if (n != (int) n) {
-                System.out.println("no es valido");
-                System.out.println("ejecuta nuevamente");
-                sw = 1;
-            } else {
-                System.out.println("correcto " + n + " es entero");
-            }
+        String nombre;
+        Double media;
+        Double basic;
+        Double pascal;
+        Double fortran;
+        System.out.println("ingrese su nombre ");
+        nombre=sc.nextLine();
+        while(nombre != "***"){
+            System.out.println("ingrese nota de basic");
+            basic=sc.nextDouble();
+            System.out.println("ingrese nota de basic");
+            pascal=sc.nextDouble();
+            System.out.println("ingrese nota de basic");
+            fortran=sc.nextDouble();
+            media=(basic + pascal + fortran) / 3;
+            System.out.println("el estudiante "+ nombre+" su nota es "+media );
+            System.out.println("ingrese su nombre ");
+            nombre=sc.nextLine();
         }
 
     }
