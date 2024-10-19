a104ae91c657350eb02e460a8eb0c6baaa43aba4
jmindineros <jmindineros@estudiante.uniajc.edu.co>
2024-10-19 22:06:22 +0000

    AVANCE ejer 5.9

--- pruebas.java
+++ pruebas.java
@@ -4,26 +4,37 @@ public class pruebas {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String art;
+        Double artA = 0.0;
+        Double artB = 0.0;
+        Double importe;
+        Double pruni;
+        Double numart;
+        Double totalA = 0.0;
+        Double totalB = 0.0;
 
-        String nombre;
-        Double media;
-        Double basic;
-        Double pascal;
-        Double fortran;
-        System.out.println("ingrese su nombre ");
-        nombre=sc.nextLine();
-        while(nombre != "***"){
-            System.out.println("ingrese nota de basic");
-            basic=sc.nextDouble();
-            System.out.println("ingrese nota de basic");
-            pascal=sc.nextDouble();
-            System.out.println("ingrese nota de basic");
-            fortran=sc.nextDouble();
-            media=(basic + pascal + fortran) / 3;
-            System.out.println("el estudiante "+ nombre+" su nota es "+media );
-            System.out.println("ingrese su nombre ");
-            nombre=sc.nextLine();
+        while (true) {
+            System.out.println("ingrese el articulo al cual quiere ingresar el dato sea A o B y para salir es X");
+            art = sc.nextLine();
+            
+            if (art.equals("X")) {
+                break;
+            }
+
+            System.out.println("ingresa el precio unitario");
+            pruni = sc.nextDouble();
+            System.out.println("ingresa el numero del articulo");
+            numart = sc.nextDouble();
+            if (art.equals("A")) {
+                artA += numart;
+                totalA += (numart * pruni);
+            } else if (art.equals("B")) {
+                artB += numart;
+                totalB += (numart * pruni);
+            } 
+
         }
-
+        System.out.println("el total de articulo a es " + artA + "y su total dee importe es" + totalA);
+        System.out.println("el total de articulo a es " + artB + "y su total de importe es" + totalB);
     }
 }
