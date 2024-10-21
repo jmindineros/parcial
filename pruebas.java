f3c18b11ce0cc0c223b154ed938e415cc5c0056c
jmindineros <jmindineros@estudiante.uniajc.edu.co>
2024-10-21 01:43:33 +0000

    5.10 finalizado

--- pruebas.java
+++ pruebas.java
@@ -4,38 +4,51 @@ public class pruebas {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String art;
-        Double artA = 0.0;
-        Double artB = 0.0;
-        Double importe;
-        Double pruni;
-        Double numart;
-        Double totalA = 0.0;
-        Double totalB = 0.0;
+
+        Double tmax;
+        double contadormax = 0.0;
+        double contadormin = 0.0;
+        Double tmin;
+        Double diasc=0.0;
+        Double mmax;
+        Double mmin;
+        Double incorrectas = 0.0;
+        System.out.println("para salir ingresa 0 en las dos temperaturas");
+        System.out.println("");
 
         while (true) {
-            System.out.println("ingrese el articulo al cual quiere ingresar el dato sea A o B y para salir es X");
-            art = sc.nextLine();
-            
-            if (art.equals("X")) {
-                break;
-            }
 
-            System.out.println("ingresa el precio unitario");
-            pruni = sc.nextDouble();
-            System.out.println("ingresa el numero del articulo");
-            numart = sc.nextDouble();
+            System.out.println("ingresa temperatura maxima");
+            tmax = sc.nextDouble();
+            System.out.println("ingresa temperatura minima");
+            tmin = sc.nextDouble();
             sc.nextLine();
-            if (art.equals("A")) {
-                artA += numart;
-                totalA += (numart * pruni);
-            } else if (art.equals("B")) {
-                artB += numart;
-                totalB += (numart * pruni);
-            } 
+            if (tmax==0 && tmin==0) {
+                break;
 
+            }
+            if (tmax == 9 || tmin == 9) {
+                incorrectas += 1;
+                System.out.println("no se puede ingresar 9 en ninguna de las temperaturas");
+                System.out.println("intentalo de nuevo");
+
+            }else{
+                diasc+=1;
+            
+            }if (tmax != 9) {
+                contadormax += tmax;
+            }if (tmin != 9) {
+                contadormin += tmin;
+            }
         }
-        System.out.println("el total de articulo a es: " + artA + " y su total dee importe es: " + totalA);
-        System.out.println("el total de articulo a es: " + artB + " y su total de importe es: " + totalB);
+
+
+        System.out.println("el numero de dias es ingresado es: "+diasc);
+        System.out.println("la media de temperatura maxima es: "+contadormax/diasc);
+        System.out.println("la media de temperatura minima es: "+contadormin/diasc);
+        System.out.println("el numero de incorrectas es "+incorrectas);
+        System.out.println("el porcentaje de las incorrectases "+(incorrectas/diasc+incorrectas)*100 );
+
+
     }
 }
